21bc28fa721557ea5eb5a76711d8cf17ff9557f7
2281 <askarbin@rambler.ru>
2021-10-09 18:34:54 +0300

    back + recognizer communication

--- service/__main__.py
+++ service/__main__.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, request
 import http, logging
-
+import httpx
 
 app = Flask(__name__)
 
@@ -8,8 +8,18 @@ app = Flask(__name__)
 @app.route("/api/v1/messages", methods = ['POST'])
 def get_messages():
     data = request.json
-    message = data['message']  
+    message = data['message']
     logging.info(message)
+    return '', http.HTTPStatus.NO_CONTENT
+
+
+@app.route("/api/v1/emotions", methods = ['POST'])
+def get_emotions():
+    data = request.json
+    text = data['text']
+    payload = {'text': text}
+    emotion = httpx.post('http://127.0.0.1:5000/api/v1/predict', json = payload)
+    logging.info('%s: %s\n\n', emotion.json()['emotions'], text)
     return '', http.HTTPStatus.NO_CONTENT
 
 
