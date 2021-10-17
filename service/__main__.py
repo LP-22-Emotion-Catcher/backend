3a98a61607d2a514c925aaf702df646eca327dad
Evgenij Karpov <gedrip87@gmail.com>
2021-10-18 00:14:54 +0300

    we add emotion to our post and save to db

--- service/__main__.py
+++ service/__main__.py
@@ -5,18 +5,26 @@ import logging
 from flask import Flask, request
 from service.config import emotion_url
 
+from database.loader import save_wall, save_post
+from database.queries import get_wall, get_post
+
 app = Flask(__name__)
 
 
 @app.route("/api/v1/messages", methods=['POST'])
 def process_message():
     data = request.json
-    print(data)
     text = data['text']
     payload = {'text': text}
     emotion = httpx.post(emotion_url, json=payload)
     logging.info('%s: %s\n\n', emotion.json()['emotions'], text)
-
+    data['emotion'] = emotion.json()['emotions']
+    response = get_wall(data['uid'])
+    if not response:
+        save_wall(data)
+    #response = get_wall(data['text'])
+    #if not response:
+    #    save_post(data)
     # TODO: save result and message to database
     return '', http.HTTPStatus.NO_CONTENT
 
