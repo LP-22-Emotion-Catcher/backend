d3813ad1e4b21c9bd20d6ccb61bc8d343ea1838c
Evgenij Karpov <gedrip87@gmail.com>
2021-10-18 00:13:56 +0300

    started developing queries for db

---
+++ database/queries.py
@@ -0,0 +1,13 @@
+from database.models import Wall, Post
+
+
+def get_wall(wall):
+    pass
+    #wall = Wall.query.filter(Wall.name == wall).first()
+    #return wall
+
+
+def get_post(post):
+    pass
+    #post = Post.query.filter(Post.name == post).first()
+    #return post
